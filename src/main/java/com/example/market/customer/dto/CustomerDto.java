d92f960a20ebe201c38c38f8c75e362dde0a65b7
Jongwoo Choi <muvnelik@naver.com>
2024-02-05 16:31:46 +0900

    [FEATURE] Customer 회원가입 관련 개발 Dto, Repository, Service, Entity 생성 회원 가입 api만 개발하였음.

--- src/main/java/com/example/market/customer/dto/CustomerDto.java
+++ src/main/java/com/example/market/customer/dto/CustomerDto.java
@@ -1,4 +1,39 @@
 package com.example.market.customer.dto;
 
+import com.example.market.customer.entity.Customer;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class CustomerDto {
+
+    private String customerName;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+    private String phoneNumber;
+    private String email;
+
+    private Set<AuthorityDto> authorityDtoSet;
+
+    public static CustomerDto from(Customer customer) {
+        if (customer == null) return null;
+
+        return CustomerDto.builder()
+                .customerName(customer.getCustomerName())
+                .phoneNumber(customer.getPhoneNumber())
+                .authorityDtoSet(customer.getAuthorities().stream()
+                        .map(authority -> AuthorityDto.builder().authorityName(authority.getAuthorityName()).build())
+                        .collect(Collectors.toSet()))
+                .build();
+    }
 }
--- src/main/java/com/example/market/customer/entity/Customer.java
+++ src/main/java/com/example/market/customer/entity/Customer.java
@@ -1,4 +1,43 @@
 package com.example.market.customer.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "customer")
 public class Customer {
+
+    @Id
+    @Column(name = "customer_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "customer_name", unique = true, nullable = false)
+    private String customerName;
+
+    @Column(name = "customer_password", nullable = false)
+    private String password;
+
+    @Column(name = "customer_phone_number", unique = true)
+    private String phoneNumber;
+
+    @Column(name = "customer_email", nullable = false)
+    private String email;
+
+    @ManyToMany
+    @JoinTable(name = "customer_authority", joinColumns = {@JoinColumn(name = "customer_id", referencedColumnName = "customer_id")},
+    inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "authority_name")})
+    private Set<Authority> authorities;
+
 }
+
+//    todo: order, cart 와 연결관게 설정하기
