069f24b372bf3d4aea679337d383c78bda625949
Jongwoo Choi <muvnelik@naver.com>
2024-02-07 21:17:42 +0900

    [FEATURE] 로그인 (인증) 관련 컨트롤러 api 구현

--- src/main/java/com/example/market/customer/controller/CustomerController.java
+++ src/main/java/com/example/market/customer/controller/CustomerController.java
@@ -1,13 +1,24 @@
 package com.example.market.customer.controller;
 
 import com.example.market.customer.dto.CustomerDto;
+import com.example.market.customer.dto.LoginRequestDto;
 import com.example.market.customer.service.CustomerService;
+import com.example.market.security.dto.TokenDto;
+import com.example.market.security.jwt.JwtFilter;
+import com.example.market.security.jwt.TokenProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,9 +26,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
 
     private final CustomerService customerService;
+    private final TokenProvider tokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
     @PostMapping("/register")
     public ResponseEntity<CustomerDto> register(@RequestBody CustomerDto requestDto) {
         return ResponseEntity.ok(customerService.register(requestDto));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDto> login(@Valid @RequestBody LoginRequestDto loginRequestDto) {
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginRequestDto.getCustomerName(), loginRequestDto.getPassword());
+
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        String jwt = tokenProvider.createToken(authentication);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
+        return new ResponseEntity<>(new TokenDto(jwt), httpHeaders, HttpStatus.OK);
     }
 }
