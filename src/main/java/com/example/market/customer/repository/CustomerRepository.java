024e7f898bebf1aeafa8acb389ad1b3bb1479c8b
Jongwoo Choi <muvnelik@naver.com>
2024-02-07 13:20:44 +0900

    [FEATURE] CustomUserDetails 관련 설정

--- src/main/java/com/example/market/customer/repository/CustomerRepository.java
+++ src/main/java/com/example/market/customer/repository/CustomerRepository.java
@@ -1,14 +1,18 @@
 package com.example.market.customer.repository;
 
 import com.example.market.customer.entity.Customer;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    Optional<Customer> findByCustomerName(String Customer);
+    Optional<Customer> findByCustomerName(String customerName);
     Optional<Customer> findByEmail(String email);
     Optional<Customer> findByPhoneNumber(String email);
+
+    @EntityGraph(attributePaths = "authorities")
+    Optional<Customer> findOneWithAuthoritiesByCustomerName(String customerName);
 
 
 }
