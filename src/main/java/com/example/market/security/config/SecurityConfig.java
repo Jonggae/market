575ec7ce3ab2ea8cae21cabcc630df5912b51f7b
Jongwoo Choi <muvnelik@naver.com>
2024-02-12 01:08:26 +0900

    [FEATURE] SecurityConfig 에 JwtAuthenticationFilter 적용

--- src/main/java/com/example/market/security/config/SecurityConfig.java
+++ src/main/java/com/example/market/security/config/SecurityConfig.java
@@ -1,12 +1,19 @@
 package com.example.market.security.config;
 
+import com.example.market.security.handler.LoginFailureHandler;
+import com.example.market.security.handler.LoginSuccessHandler;
 import com.example.market.security.jwt.JwtAccessDeniedHandler;
 import com.example.market.security.jwt.JwtAuthenticationEntryPoint;
+import com.example.market.security.jwt.LoginProvider;
 import com.example.market.security.jwt.TokenProvider;
+import com.example.market.security.utils.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,6 +22,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -25,10 +33,31 @@ public class SecurityConfig {
     private final TokenProvider tokenProvider;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final LoginProvider loginProvider;
+    private final LoginSuccessHandler successHandler;
+    private final LoginFailureHandler failureHandler;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
+        JwtAuthenticationFilter filter = new JwtAuthenticationFilter(tokenProvider, loginProvider, successHandler, failureHandler);
+        filter.setAuthenticationManager(authenticationManager(authenticationConfiguration));
+        return filter;
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        return loginProvider;
     }
 
     @Bean
@@ -44,10 +73,14 @@ public class SecurityConfig {
         http.sessionManagement(sessionManagement ->
                 sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-        http.authorizeHttpRequests((authorizeHttpRequest ->authorizeHttpRequest
+        http.authorizeHttpRequests((authorizeHttpRequest -> authorizeHttpRequest
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                        .antMatchers("/api/customer/register", "/api/customer/login").permitAll()
-                        .anyRequest().authenticated()));
+                .antMatchers("/api/customer/register", "/api/customer/login").permitAll()
+                .anyRequest().authenticated()));
+
+        http.authenticationProvider(loginProvider);
+
+        http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 
         http.apply(new JwtSecurityConfig(tokenProvider)); // 다른 버전에 유의
 
