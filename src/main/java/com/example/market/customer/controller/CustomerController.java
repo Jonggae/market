a9219d3c9e4d8ca3bffc714716eb4c10a5c61d46
Jongwoo Choi <muvnelik@naver.com>
2024-02-12 01:07:12 +0900

    [FEATURE] 로그인 과정을 Security 내에서 LoginProvider 로 처리함 Controller login api 는 JwtAuthenticationFilter 로 넘어감. 로그인 성공, 실패 로직을 결정하는 핸들러도 생성

--- src/main/java/com/example/market/customer/controller/CustomerController.java
+++ src/main/java/com/example/market/customer/controller/CustomerController.java
@@ -1,24 +1,13 @@
 package com.example.market.customer.controller;
 
 import com.example.market.customer.dto.CustomerDto;
-import com.example.market.customer.dto.LoginRequestDto;
 import com.example.market.customer.service.CustomerService;
-import com.example.market.security.dto.TokenDto;
-import com.example.market.security.jwt.JwtFilter;
-import com.example.market.security.jwt.TokenProvider;
+import com.example.market.security.utils.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,25 +15,19 @@ import javax.validation.Valid;
 public class CustomerController {
 
     private final CustomerService customerService;
-    private final TokenProvider tokenProvider;
-    private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
     @PostMapping("/register")
     public ResponseEntity<CustomerDto> register(@RequestBody CustomerDto requestDto) {
         return ResponseEntity.ok(customerService.register(requestDto));
     }
 
-    @PostMapping("/login")
-    public ResponseEntity<TokenDto> login(@Valid @RequestBody LoginRequestDto loginRequestDto) {
-        UsernamePasswordAuthenticationToken authenticationToken =
-                new UsernamePasswordAuthenticationToken(loginRequestDto.getCustomerName(), loginRequestDto.getPassword());
+    // todo : Basic header로 CustomerName, Password 받아오기
 
-        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
 
-        String jwt = tokenProvider.createToken(authentication);
-
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
-        return new ResponseEntity<>(new TokenDto(jwt), httpHeaders, HttpStatus.OK);
+    // 로그인 한 개인이 접근 가능한 내 정보 페이지
+    @GetMapping("/customer-info")
+    @PreAuthorize("hasAnyRole('USER','ADMIN')")
+    public CustomerDto getCustomerInfo(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        return customerService.getCustomerInfo();
     }
 }
