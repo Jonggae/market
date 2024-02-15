d028a8e5379e054512cd7b6f033872a8e6fefa17
Jongwoo Choi <muvnelik@naver.com>
2024-02-15 14:18:47 +0900

    [FIX] 암호화 로직 512로 변경

--- src/main/java/com/example/market/security/jwt/TokenProvider.java
+++ src/main/java/com/example/market/security/jwt/TokenProvider.java
@@ -58,7 +58,7 @@ public class TokenProvider implements InitializingBean {
         return Jwts.builder()
                 .setSubject(authentication.getName())
                 .claim(AUTHORITIES_KEY, authorities)
-                .signWith(key, SignatureAlgorithm.HS256)
+                .signWith(key, SignatureAlgorithm.HS512)
                 .setExpiration(validity)
                 .compact();
     }
