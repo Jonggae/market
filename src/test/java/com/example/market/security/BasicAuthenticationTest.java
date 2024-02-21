3a3016601f0b38f5d48e71a19bafb1d6a4e431d9
Jongwoo Choi <muvnelik@naver.com>
2024-02-21 22:46:51 +0900

    [FEATURE] Basic header 관련 다시 시작 우선 관련 내용은 삭제하고 어디에 넣을 수 있을지 고려

--- src/test/java/com/example/market/security/BasicAuthenticationTest.java
+++ src/test/java/com/example/market/security/BasicAuthenticationTest.java
@@ -28,19 +28,7 @@ public class BasicAuthenticationTest {
     @Test
     @DisplayName("Basic header를 받아 로그인 시 JWT 발급되는지 테스트")
     public void basicHeaderTest() throws Exception {
-        String encodedCredentials = Base64.getEncoder().encodeToString("testUser:password".getBytes());
 
-        MvcResult mvcResult = mockMvc.perform(post("/api/customer/login")
-                        .header("Authorization", "Basic " + encodedCredentials)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andReturn();
-
-        String responseBody = mvcResult.getResponse().getContentAsString();
-        String token = JsonPath.parse(responseBody).read("$.token", String.class); // 토큰 값 추출
-        System.out.println("Extracted JWT Token: " + token); // 콘솔에 토큰 출력
-
-        assertNotNull(token);
     }
 }
 
