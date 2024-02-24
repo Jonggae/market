b674eb1a4d26deec44cb33692a7cb9963fa1a6f4
Jongwoo Choi <muvnelik@naver.com>
2024-02-24 22:35:40 +0900

    [FEATURE] TestConfiguration 설정

--- src/test/java/com/example/market/config/TestDataInitializerConfig.java
+++ src/test/java/com/example/market/config/TestDataInitializerConfig.java
@@ -5,14 +5,14 @@ import com.example.market.customer.entity.Customer;
 import com.example.market.customer.repository.AuthorityRepository;
 import com.example.market.customer.repository.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Collections;
 
-@Configuration
+@TestConfiguration
 @Profile("test")
 public class TestDataInitializerConfig {
     @Bean
