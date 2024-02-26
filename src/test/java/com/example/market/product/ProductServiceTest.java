5543e06d2674901f038d007b5ac4dd5cae38c212
Jongwoo Choi <muvnelik@naver.com>
2024-02-26 16:51:35 +0900

    [FIX] 예외 처리에 해당하는 테스트 추가

--- src/test/java/com/example/market/product/ProductServiceTest.java
+++ src/test/java/com/example/market/product/ProductServiceTest.java
@@ -51,15 +51,25 @@ public class ProductServiceTest {
 
     @Test
     @Transactional
-    @DisplayName("상품 등록 테스트")
+    @DisplayName("관리자(ADMIN) 상품 등록 테스트")
     @WithMockUser(roles = "ADMIN")
-    void addProductTest() throws Exception {
+    void addProductTestWithADMIN() throws Exception {
         ProductDto newProduct = new ProductDto(null, "New Product", "New Description", 2000L, 20L);
         mockMvc.perform(post("/api/products")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(newProduct)))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.productName").value("New Product"));
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value(newProduct.getProductName() + " 해당 상품 등록이 완료되었습니다."));
+    } @Test
+    @Transactional
+    @DisplayName("일반 사용자(USER) 상품 등록 테스트")
+    @WithMockUser(roles = "USER")
+    void addProductTestWithCustomer() throws Exception {
+        ProductDto newProduct = new ProductDto(null, "New Product", "New Description", 2000L, 20L);
+        mockMvc.perform(post("/api/products")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(newProduct)))
+                .andExpect(status().isForbidden());
     }
 
     @Test
@@ -83,6 +93,18 @@ public class ProductServiceTest {
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.productName").value("테스트 상품"));
+    }
+
+    @Test
+    @DisplayName("상품 단일 조회 실패 테스트")
+    @Transactional
+    void failShowOneProductTEst() throws Exception{
+        long nonExistProductId = 333;
+
+        mockMvc.perform(get("/api/products/" + nonExistProductId)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("상품이 존재하지 않습니다."));
     }
 
     @Test
