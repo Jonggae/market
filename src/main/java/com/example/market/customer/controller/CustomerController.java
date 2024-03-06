34f6b7b1dd1cd717b9d9329f471d39b3b1d6f4a0
Jongwoo Choi <muvnelik@naver.com>
2024-03-06 17:26:49 +0900

    [FIX] Customer 관련 ApiResponse 수정한 내용으로 변경 / 테스트코드는 추후 생성 (누락 사항 있을 수 있음)

--- src/main/java/com/example/market/customer/controller/CustomerController.java
+++ src/main/java/com/example/market/customer/controller/CustomerController.java
@@ -1,11 +1,15 @@
 package com.example.market.customer.controller;
 
+import com.example.market.commons.apiResponse.ApiResponseDto;
+import com.example.market.commons.apiResponse.ApiResponseUtil;
 import com.example.market.customer.dto.CustomerDto;
 import com.example.market.customer.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,14 +19,18 @@ public class CustomerController {
     private final CustomerService customerService;
 
     @PostMapping("/register")
-    public ResponseEntity<CustomerDto> register(@RequestBody CustomerDto requestDto) {
-        return ResponseEntity.ok(customerService.register(requestDto));
+    public ResponseEntity<ApiResponseDto<CustomerDto>> register(@RequestBody CustomerDto requestDto) {
+        CustomerDto registerdCustomerDto = customerService.register(requestDto);
+        String successMessage = "회원 가입이 성공적으로 완료되었습니다.";
+        return ApiResponseUtil.success(successMessage, registerdCustomerDto, 200);
     }
 
     // 로그인 한 개인이 접근 가능한 내 정보 페이지
     @GetMapping("/my-info")
     @PreAuthorize("hasAnyRole('USER','ADMIN')")
-    public CustomerDto getCustomerInfo() {
-        return customerService.getCustomerInfo();
+    public ResponseEntity<ApiResponseDto<CustomerDto>> getCustomerInfo() {
+        CustomerDto getCustomerDto = customerService.getCustomerInfo();
+        String successMessage = "해당 유저의 정보입니다";
+        return ApiResponseUtil.success(successMessage, getCustomerDto, 200);
     }
 }
